94041a0b9a22e5d09d1e5e2d988570a1aad271d9
Malathi <57785954+Malathi1993@users.noreply.github.com>
2020-06-08 12:48:32 +0530

    modify

--- app/src/main/java/com/example/fewscreensex/RegisterActivity.java
+++ app/src/main/java/com/example/fewscreensex/RegisterActivity.java
@@ -8,22 +8,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
-EditText Email;
-EditText Password;
-EditText MobileNo;
-Button Login;
-TextView Rigisterdlogin;
+    EditText Email;
+    EditText Password;
+    EditText MobileNo;
+    Button Login;
+    TextView Rigisterdlogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Rigisterdlogin=(TextView)findViewById(R.id.textView);
-        Email=(EditText)findViewById(R.id.editText);
-        Password=(EditText)findViewById(R.id.editText2);
-        MobileNo=(EditText)findViewById(R.id.editText3);
-        Login=(Button)findViewById(R.id.button);
+        Rigisterdlogin = (TextView) findViewById(R.id.textView);
+        Email = (EditText) findViewById(R.id.editText);
+        Password = (EditText) findViewById(R.id.editText2);
+        MobileNo = (EditText) findViewById(R.id.editText3);
+        Login = (Button) findViewById(R.id.button);
 
         Rigisterdlogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,22 +33,18 @@ TextView Rigisterdlogin;
                 if (Email.getText().toString().length() == 0) {
                     Email.setError("Email not entered");
                     Email.requestFocus();
-                }
-
-                else if (Password.getText().toString().length() == 0) {
+                }  if (Password.getText().toString().length() == 0) {
                     Password.setError("Password not entered");
                     Password.requestFocus();
-                }
-                else if (MobileNo.getText().toString().length() == 0) {
+                }  if (MobileNo.getText().toString().length() == 0) {
                     MobileNo.setError("PhoneNO is Required");
                     MobileNo.requestFocus();
                 }
-                else (Login.getText().toString().length() == 0) {
-                    Login.setError("Login Credentionals not entered");
-                    Login.requestFocus();
+                else {
+                    Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+                    Intent it = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(it);
                 }
-                Intent it=new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(it);
             }
         });
     }
